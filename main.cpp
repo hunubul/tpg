ed0e6c4192e2ff345ded8c92099e6bca4b737ac2
kote2ster <kote2ster@gmail.com>
2014-09-27 21:33:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
-#include "szoba.cpp"
+#include "szoba.h"
 
 using namespace std;
-void Kiir(szoba asd)
-{
-    for(int i=0;i<asd.f.size();i++)
-    {
-        cout<<asd.f[i]<<endl;
-    }
-}
+void Kiir(szoba asd); //fv-ek prototípusait itt kell feltüntetni (Ez standard programozási követelmény), ezek lehet hogy később átkerülhetnének egy új header-be ha sok lesz belőlük
 int main()
 {
     srand(time(NULL));
     szoba asd;
     Kiir(asd);
     return 0;
+}
+void Kiir(szoba asd)
+{
+    for(int i=0;i<asd.f.size();i++)
+    {
+        cout<<asd.f[i]<<endl;
+    }
 }

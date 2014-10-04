4ef774707b06f80823278d8a3f42f5a84ce81494
kote2ster <kote2ster@gmail.com>
2014-10-04 14:44:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "szoba.h"
 
 using namespace std;
-//fv-ek prototípusait itt kell feltüntetni (Ez standard programozási követelmény), ezek lehet hogy később átkerülhetnének egy új header-be ha sok lesz belőlük
-//void Kiir(szoba asd); //TAGFUGGVENY
 
 int main()
 {

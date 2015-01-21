2bd9181823a7ccf3c34c7549df6608b80a68040e
Ákos Köte <kote2ster@gmail.com>
2015-01-21 18:40:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@
 #include "main.h"
 
 using namespace std;
+int ConsoleWidth,ConsoleHeight;
+int FontX=8;
+int FontY=8;
 
 int main() {
     srand(time(NULL));
+    TCODSystem::getCurrentResolution(&ConsoleWidth,&ConsoleHeight);
+    ConsoleWidth /= FontX;
+    ConsoleHeight /= FontY;
     TCODConsole::initRoot(ConsoleWidth,ConsoleHeight,"TPG");
+    #ifndef DEBUG
+    TCODConsole::setFullscreen(true);
+    #endif // DEBUG
     level asd(17,17,8,8,80); /* A pálya inicializálása */
     chest Chest; /* Chest inicializálás */
     asd.engine(); /* A fõ loop */

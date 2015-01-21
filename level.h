debdfe4929455814168ea7629060d034beed691b
Ákos Köte <kote2ster@gmail.com>
2015-01-21 18:42:29 +0100

    Update level.h

--- level.h
+++ level.h
@@ -11,9 +11,11 @@ private:
     void writearrow();
 public:
     std::vector<std::vector<int> > Map;
+    std::vector<std::vector<int> > FOV;
     std::vector<std::vector<szoba> > terkep; //egyértelmû
     level(int Max_X,int Max_Y,int MidX,int MidY,int MaxCounter);
     void engine();
     void writeout(int posX,int posY);
+    void RoomWriteout();
 };
 #endif

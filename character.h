2c20aeb8a638e52772fb86bbe2dbd4c3aa4b13da
hunubul <ubul1111@hotmail.com>
2015-01-29 19:05:30 +0100

    karakter class

--- character.h
+++ character.h
@@ -1,6 +1,11 @@
 #ifndef CHARACTER_H_INCLUDED
 #define CHARACTER_H_INCLUDED
 
+typedef struct {
+    std::string effect; //blind bleed poison satöbbi
+    int roundsleft; //mennyi körig van még fent az effect
+} balfasz;
+
 class character{
 private:
   int maxhp; //egyértelmű
@@ -9,8 +14,9 @@ private:
   int stam; //jelenlegi stamina
   int defense; //védelmi érték
   int offense; //támadó érték
-  vector<ITEMS> inventory; /táska tartalma
-  vector<ITEMS> wearing; //amit hord, ez külön kezelendő inventorytól
+  std::vector<balfasz> status; //blind bleed etc ide, struktúra feljebb
+  std::vector<ITEMS> inventory; //táska tartalma
+  std::vector<ITEMS> wearing; //amit hord, ez külön kezelendő inventorytól
 public:
   character(); //konstruktor, jelenleg üres
 };

15616705fbd94214a2d9e791d34f5e3a42d2be14
hunubul <ubul1111@hotmail.com>
2015-02-17 11:58:44 +0100

    Update harc.cpp

--- harc.cpp
+++ harc.cpp
@@ -1,5 +1,10 @@
 #include "main.h"
 harc::harc(character player, character enemy, std::string prefdef, std::string prefatc){
+  p=player;
+  e=enemy;
+  def=predef;
+  atc=prefatc;
+  most=0;
 }
 harc::~harc{
 }

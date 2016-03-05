03ee2be79886eab6c7f2305f7fe54107b7656c23
kote2ster <kote2ster@gmail.com>
2016-03-05 21:11:49 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void BeolvasEnemyk();
 int main() {
 	//StartTimeStartup=boost::posix_time::microsec_clock::local_time();
 	srand(time(NULL));
-	//TCODSystem::getCurrentResolution(&ConsoleWidth, &ConsoleHeight);
+	TCODSystem::getCurrentResolution(&ConsoleWidth, &ConsoleHeight);
 	ConsoleWidth = 1024;
 	ConsoleHeight = 768;
 	ConsoleWidth /= FontX;

6150041f29cfd86ff88074407811502fc20a6560
kote2ster <kote2ster@gmail.com>
2016-06-29 00:29:14 +0200

    a

--- opengl/OpenGLRender.cpp
+++ opengl/OpenGLRender.cpp
@@ -60,6 +60,14 @@ void initAll()
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	//glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
 
+	/*GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+
+	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);*/
+
 	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
 	glfwMakeContextCurrent(window);
 
